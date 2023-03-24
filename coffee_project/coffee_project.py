3ed8beb18fe2e0df10d91a289f969bfc7f5504fe
ArtemisGold <so.info@mailbox.org>
2023-03-24 22:15:00 +0100

    Update coffee_project.py
    Format Dataframe

--- coffee_project/coffee_project.py
+++ coffee_project/coffee_project.py
@@ -3,15 +3,25 @@
 # Classification of coffee spectra
 # The file "FTIR_Spectra_instant_coffee.csv" contains a collection of 56 mid 
 # infrared diffuse reflectance (MIR-DRIFT) spectra of lyophilized coffee 
-# produced from two species: arabica (29 samples) and canephora var. 
-# robusta (27 samples).
+# produced from two species: arabica (29 samples, '1') and canephora var. 
+# robusta (27 samples, '2').
 
-import numpy as np
+
 import pandas as pd
-#pd.options.display.max_columns = 8
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("C:\\Users\\sophi\\Documents\\GitHub\\Spectroscopy\\coffee_project\\data\\FTIR_Spectra_instant_coffee.csv")
-df = df.transpose()
-print(df.head(10))
-print(df.describe())    
+
+df.rename(columns = {'Sample Number:':'sample'}, inplace = True)
+df.set_index('sample', inplace=True)
+df_t = df.transpose()
+df_t.drop(df_t.columns[[1]], axis=1, inplace=True)
+df_t.rename(columns = {'Group Code:':'target'}, 
+            inplace = True)
+
+print(df_t.head(10)) 
+
+y = df_t["target"].values
+# X = df_t[[1:57]] #TODO X-Daten definieren
+
+
